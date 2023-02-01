58ad70f66ea99ff772e8eabc170fb0ad34dd8655
websorcier <dev.websorcier@gmail.com>
2023-02-01 10:50:46 +0530

    config updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/miniapps-whatsapp-nosave/',
+	// base: '/miniapps-whatsapp-nosave/',
 	plugins: [svelte()],
 });
